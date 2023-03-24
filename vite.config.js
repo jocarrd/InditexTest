afee18bf87861356bbe3ff1381d29a8af30211c1
jocarrd <jorgecarrera98d@gmail.com>
2023-03-24 09:14:24 +0100

    feat: add pretier eslint and vite config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import legacy from "@vitejs/plugin-legacy";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 /*
 El plugin vite-plugin-legacy se encarga concatenar y minimizar los archivos de assets en un solo archivo. 
 Además tambien genera una versión compatible con navegadores más antiguos, y además optimiza el tamaño del archivo de assets. 
-
-
 */
 export default defineConfig({
   plugins: [
     react(),
     legacy({
-      targets: ["defaults", "not IE 11"],
-      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
-      polyfills: ["es.promise.finally"],
+      targets: ['defaults', 'not IE 11'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+      polyfills: ['es.promise.finally'],
     }),
   ],
-  mode: process.env.NODE_ENV || "development",
-});
+  mode: process.env.NODE_ENV || 'development',
+})
