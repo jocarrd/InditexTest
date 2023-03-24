de056909365cd63642d111dd8d83f13aca60981b
jocarrd <jorgecarrera98d@gmail.com>
2023-03-24 14:53:58 +0100

    feat: rounded podcast image

--- src/components/PodcastCard/index.tsx
+++ src/components/PodcastCard/index.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 export const PodcastCard = ({ podcast }) => {
   return (
     <div className="flex flex-col md:max-w-lg place-items-center shadow-md border-gray-300 duration-300 hover:-translate-y-1">
-      <img className="rounded-full" src={podcast['im:image'][2].label} />
+      <img
+        className="rounded-full -translate-y-12"
+        src={podcast['im:image'][2].label}
+      />
       <div> {podcast.title.label}</div>
       <div>Author: {podcast['im:artist'].label}</div>
     </div>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,13 +4,13 @@ import React from 'react'
 
 export const Home = () => {
   const {
-    state: { podcasts },
+    state: { podcasts, loading },
   } = usePodcasts()
 
   console.log(podcasts)
 
   return (
-    <div className="grid grid-flow-row grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+    <div className="grid grid-flow-row grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8  gap-y-32">
       {podcasts.map((podcast) => (
         <PodcastCard podcast={podcast} />
       ))}
