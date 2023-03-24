7ea88378f4c1dcc0f589720c8bab8a406fad5d28
jocarrd <jorgecarrera98d@gmail.com>
2023-03-24 13:53:15 +0100

    feat: add router

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,17 @@
+import { Link, Outlet } from 'react-router-dom'
+
+export const Layout = () => {
+  return (
+    <>
+      <header className="py-4 px-8 flex flex-col gap-2">
+        <Link to="/" className="text-blue-700 text-xl">
+          Podcaster
+        </Link>
+        <hr />
+      </header>
+      <main className=" flex  flex-col place-items-center">
+        <Outlet />
+      </main>
+    </>
+  )
+}
