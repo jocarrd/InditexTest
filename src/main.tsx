f328d4765f50e8f5ad6e37364c02e1507d2a6104
jocarrd <jorgecarrera98d@gmail.com>
2023-03-25 13:08:41 +0100

    feat: remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,5 @@ import './index.css'
 import { router } from './Router'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 )
